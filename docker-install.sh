c0977fc4b2d773242a5c2093098514c2e05d3fb7
raju30 <raju30.wls@gmail.com>
2020-06-29 11:31:58 +0530

    Updated docker script

--- docker-install.sh
+++ docker-install.sh
@@ -3,17 +3,23 @@ sudo apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
+    gnupg2 \
     software-properties-common
 
-curl -fsSL https://yum.dockerproject.org/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 
 sudo add-apt-repository \
-    "deb https://apt.dockerproject.org/repo/ \
-    ubuntu-$(lsb_release -cs) \
-    main"
+    "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 
 sudo apt-get update
-sudo apt-get -y install docker-engine docker-compose
+sudo apt-cache policy docker-ce
+sudo apt install -y docker-ce
+sudo systemctl status docker
 
 # add current user to docker group so there is no need to use sudo when running docker
 sudo usermod -aG docker $(whoami)
+
+# Install docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
